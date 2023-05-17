5809c946398bd5fea119c007eb8be4573f7f66e1
mariodac <mdac.mario@gmail.com>
2023-05-17 10:51:34 -0300

    Update log.py

--- modules/log.py
+++ modules/log.py
@@ -2,10 +2,23 @@ import logging
 from os import path, environ, name
 class Logger:
     def __init__(self, log_name):
+        """
+         Iniciar o log. Isto é chamado por __init__ e não deve ser chamado diretamente
+         
+         @param log_name - O nome do log
+        """
         self.log_name = log_name
         self.loggers = {}
+        
     def getLogger(self):
+        """
+         Retorna o objeto de log. Verificação para não criar vários instãncias  e gerar linhas duplicadas no log 
+         
+         
+         @return Objeto de log aqui estable
+        """
         # verificação para não criar vários objetos e gerar linhas duplicadas no log 
+        # Verifica se já existe instancia de logger
         if self.loggers.get(name):
             return self.loggers.get(name)
         else:
@@ -13,6 +26,7 @@ class Logger:
             logger = logging.getLogger(self.log_name)
             # configura nivel de log
             logger.setLevel('DEBUG')
+            # verifica sistema operacional
             if name == 'nt':
                 path_log = environ['TEMP']
                 log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
