b05bb314839e1c4eccd1994e257fb8f713298224
mariodouglasac <mdac.mario@gmail.com>
2023-10-11 19:48:33 -0300

    novas constantes

--- utils/constants.py
+++ utils/constants.py
@@ -4,3 +4,5 @@ SAIKO_URL = "https://saikoanimes.net/"
 
 ANIMEFIRENET_URL = "https://animefire.net/"
 
+AGREGADOR_MANGA = {'MANGALIVRE': ['https://mangalivre.net'], 'MANGADEX': ['https://mangadex.org', '/search?q='], 'MANGAFIRE': ['https://mangafire.to', '/filter?keyword='], 'LERMANGA': ['https://lermanga.org', '/?s='], 'MANGASCHAN': ['https://mangaschan.net', '/?s=']}
+
