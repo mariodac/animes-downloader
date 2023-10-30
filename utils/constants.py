2123e9fe47670d6acb14c96c077b94ab83b1e335
mariodouglasac <mdac.mario@gmail.com>
2023-10-30 04:20:16 -0300

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,5 +1,7 @@
 NAME_LOG = "animes_download"
 
+ANILIST = "https://anilist.co"
+
 AGREGADOR_ANIME = {"saiko" : "https://saikoanimes.net/", "animefire.net" : "https://animefire.net/"}
 
 AGREGADOR_MANGA = {'MANGALIVRE': 'https://mangalivre.net', 'MANGADEX': 'https://mangadex.org', 'MANGAFIRE': 'https://mangafire.to', 'LERMANGA': 'https://lermanga.org', 'MANGASCHAN': 'https://mangaschan.net'}
