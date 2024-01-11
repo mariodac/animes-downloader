520ad39bde74e5504c01153f697ee7c3a523118a
mariodac <mdac.mario@gmail.com>
2024-01-11 14:04:03 -0300

    retirado item não utilizado

--- utils/constants.py
+++ utils/constants.py
@@ -4,5 +4,5 @@ ANILIST = "https://anilist.co"
 
 AGREGADOR_ANIME = {"saiko" : "https://saikoanimes.net/", "animefire.net" : "https://animefire.net/"}
 
-AGREGADOR_MANGA = {'MANGALIVRE': 'https://mangalivre.net', 'MANGADEX': 'https://mangadex.org', 'API-MANGADEDX': 'https://api.mangadex.org', 'MANGAFIRE': 'https://mangafire.to', 'LERMANGA': 'https://lermanga.org', 'MANGASCHAN': 'https://mangaschan.net', 'BRMANGA': 'https://www.brmangas.net'}
+AGREGADOR_MANGA = {'MANGALIVRE': 'https://mangalivre.net', 'MANGADEX': 'https://mangadex.org', 'API-MANGADEDX': 'https://api.mangadex.org', 'LERMANGA': 'https://lermanga.org', 'MANGASCHAN': 'https://mangaschan.net', 'BRMANGA': 'https://www.brmangas.net', }
 
