11276a8f6eac3cfa574ceb4c71afa7e8af7db07b
mariodac <mdac.mario@gmail.com>
2024-01-11 17:48:08 -0300

    adicionado mais links agregadores

--- utils/constants.py
+++ utils/constants.py
@@ -4,5 +4,5 @@ ANILIST = "https://anilist.co"
 
 AGREGADOR_ANIME = {"saiko" : "https://saikoanimes.net/", "animefire.net" : "https://animefire.net/"}
 
-AGREGADOR_MANGA = {'MANGALIVRE': 'https://mangalivre.net', 'MANGADEX': 'https://mangadex.org', 'API-MANGADEDX': 'https://api.mangadex.org', 'LERMANGA': 'https://lermanga.org', 'MANGASCHAN': 'https://mangaschan.net', 'BRMANGA': 'https://www.brmangas.net', }
+AGREGADOR_MANGA = {'MANGALIVRE': 'https://mangalivre.net', 'MANGADEX': 'https://mangadex.org', 'API-MANGADEDX': 'https://api.mangadex.org', 'MANGASCHAN': 'https://mangaschan.net', 'BRMANGA': 'https://www.brmangas.net', 'LERMANGA': 'https://lermanga.org', 'MANHASTRO': 'https://manhastro.com', 'SLIMEREAD': 'https://slimeread.com', 'TSUKIMANGAS': 'https://tsuki-mangas.com'}
 
